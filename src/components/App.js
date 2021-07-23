c697ec6d245a27639a724f635e7f47f5f5532373
Leonid Lukin <leonidlukin90@gmail.com>
2021-07-23 20:53:15 +0300

    implemented 'loggin', 'signup' and 'logout' of the app. Also implemented .reset password'.

---
+++ src/components/App.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import { AuthProvider } from '../contexts/AuthContext'
+import SignUp from './SignUp'
+import { BrowserRouter as Router, Switch, Route, NavLink} from 'react-router-dom'
+import Todolist from './ToDoList'
+import Login from './Login'
+import ForgotPassword from './ForgotPassword'
+import PrivateRoute from './PrivateRoute'
+
+function App() {
+  return (
+    <Container className='d-flex align-items-center 
+      justify-content-center'
+      style={{minHeight: '100vh' }}
+    >
+      <div className='w-100' style={{maxWidth: '400px' }}>
+        <Router>
+          <nav>
+            <ul style={{display: 'flex', listStyle: 'none'}}>
+              {/* <li style={{marginRight: '10px'}}>
+                <NavLink exact to="/" activeStyle={{fontWeight: "bold", color: "red"}}>To Do List</NavLink>
+              </li> */}
+              <li style={{marginRight: '10px'}}>
+                <NavLink to="/signup" activeStyle={{fontWeight: "bold", color: "red"}}>Sign Up</NavLink>
+              </li>
+              <li style={{marginRight: '10px'}}>
+                <NavLink to="/login" activeStyle={{fontWeight: "bold", color: "red"}}>Log In</NavLink>
+              </li>
+            </ul>
+          </nav>
+          <AuthProvider>
+            <Switch>
+              <PrivateRoute exact path='/' component={Todolist} />
+              <Route path='/signup' component={SignUp} />
+              <Route path='/login' component={Login} />
+              <Route path='/forgot-password' component={ForgotPassword} />
+            </Switch>
+          </AuthProvider>
+        </Router>
+      </div>
+    </Container>
+  )
+}
+
+export default App
---
+++ src/components/ToDoList.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import { Button, Card, Alert } from 'react-bootstrap'
+import { useAuth  } from '../contexts/AuthContext'
+import { Link,useHistory } from 'react-router-dom'
+
+export default function Todolist() {
+    const [error, setError] = useState('')
+    const { currentUser, logout } = useAuth()
+    const history = useHistory()
+
+    async function handleLogout() {
+        setError('')
+
+        try {
+            await logout()
+            history.push('/login')
+        } catch {
+            setError('Не удалось выйти')
+        }
+    }
+
+    return (
+        <>
+            <Card>
+                <Card.Body>
+                    <h2 className='text-center md-4'>Profile</h2>
+                    {error && <Alert variant='danger'>{error}</Alert>}
+                    <strong>Email:</strong>{currentUser.email}
+                    <Link to='/update-profile' className='btn btn-primary w-100 mt-3'>
+                        Update Profile
+                    </Link>
+                </Card.Body>
+            </Card>
+            <div className='w-100 text-center mt-2'>
+                <Button variant='lonk' onClick={handleLogout}>Log Out</Button>
+            </div>
+        </>
+    )
+}
